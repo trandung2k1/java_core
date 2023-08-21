345df65358d25f19625803d29ac6d6ce99fea4dc
trandung2k1 <trandungitksnb2001@gmail.com>
2023-08-21 22:46:54 +0700

    add Day01

---
+++ Day01/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Hello Java");
+        System.out.print("Hello Dung");
+        System.out.print("Hello Mai");
+        System.out.println();
+        System.out.println("2" + 2);
+    }
+}
